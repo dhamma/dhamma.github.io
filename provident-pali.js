58f1130c8be4b27a7642aa2470c9c50e22571f8d
yapcheahshen@gmail.com <yapcheahshen@gmail.com>
2021-12-06 09:56:38 +0630

    add various indic transliteration

--- provident-pali.js
+++ provident-pali.js
@@ -211,64 +211,7 @@ var providentpali = (function (exports) {
         return out;
     };
 
-    //IPA Transcript from the World Tipitaka Edition 2009
-    const p2e={
-        'a':'a','A':'a:','I':'i','II':'i:','U':'u','UU':'u:','e':'e','o':'o',
-        'aA':'a:','iI':'i:','uU':'u:','E':'e',
-        'k':'k','K':'kh','g':'g','G':'gh','NVk':'Nk','NVK':'Nkh','NVg':'Ng','NVG':'Ngh',
-        'c':'c','C':'ch','j':'J','J':'Jh','Y':'n^', 'y':'j',
-        'W':'t','X':'th','F':'d.','Q':'d.h','N':'n.','r':'r','L':'l.',
-        't':'t[','T':'t[h','d':'d[','D':'d[h','n':'n[','l':'l[','s':'s[',
-        'p':'p','P':'ph','b':'b','B':'bh','m':'m',
-        'v':'v',
-        'M':'~',
-
-    };
-
-    const toESpeak=(str,opts={})=>{
-        let out='';
-        const convert=p=>{
-            let s='',idx=0,needVowel=false;
-            while (idx<p.length) {
-                let sub=p[idx];
-                const v='aeiouMAEIOU'.indexOf(sub);
-                needVowel= (v==-1) ;
-                while (idx<p.length && p2e[sub+p[idx+1]]) {
-                    idx++;
-                    sub+=p[idx];
-                }
-                idx++;
-                
-                if (needVowel && p[idx]&& 'aeiouMEIOU'.indexOf(p[idx])==-1 ) {
-                    s+='a';
-                }
-                s+=p2e[sub];
-                sub='';
-            }
-            return s;
-        };
-
-        const parts=(opts.format==='xml')?str.split(/(<[^<]+>)/):[str];
-        for (let j=0;j<parts.length;j++) {
-            if (parts[j][0]=='<') {
-                out+=parts[j];
-                continue;
-            }
-            const units=parts[j].split(/([aeiouVBCDFGHJKLNPQRSTVWXYZbcdghjklmnprstuvyAEIOUM]+)/);
-            units.forEach(s=>{
-                const m=s.match(/[aeiouVBCDFGHJKLNPQRSTVWXYZbcdghjklmnprstuvyAEIOUM]/);
-                if (!m) {
-                    out+=s;
-                } else {
-                    out+=convert(s);    
-                }
-            });
-        }
-
-        return out;
-    };
-
-    const mapping={
+    const devanagari={
         'क':'k','ख':'K','ग':'g', 'घ':'G','ङ':'N', 'ह':'h',
         'च':'c','छ':'C','ज':'j','झ':'J','ञ':'Y','य':'y','श':'Z',
         'ट':'W','ठ':'X','ड':'F','ढ':'Q','ण':'N','र':'r','ष':'S',
@@ -280,13 +223,120 @@ var providentpali = (function (exports) {
         '्':'V', //virama , 連接下個輔音。
         '०':'0','१':'1','२':'2','३':'3','४':'4','५':'5','६':'6','७':'7','८':'8','९':'9',
         '।':'|','॥':'||',
-        'ौ':'!!au', //invalid in pali
-        'ै' :'!!ai',//invalid in pali
-        'ऋ':'!!R',
-        'ः':'!!H'//visarga, rare
+        'ौ':'aU', //invalid in pali
+        'ै' :'aI',//invalid in pali
+        'ऋ':'R',
+        'ः':'H'//visarga, rare
     };
 
+    const sinhala={
+       'ක':'k','ඛ':'K','ග':'g', 'ඝ':'G','ඞ':'N', 'හ':'h',
+       'ච':'c','ඡ':'C','ජ':'j','ඣ':'J','ඤ':'Y','ය':'y','श':'Z',
+       'ට':'W','ඨ':'X','ඩ':'F','ඪ':'Q','ණ':'N','ර':'r','ष':'S', 
+       'ත':'t','ථ':'T','ද':'d','ධ':'D','න':'n','ල':'l','ස':'s', 
+       'ප':'p','ඵ':'P','බ':'b','භ':'B','ම':'m','ව':'v','ළ':'L','ං':'M',
+       'අ':'a','ඉ':'i','උ':'u','එ':'e','ඔ':'o','ආ':'aA','ඊ':'iI','ඌ':'uU',
+       'ා':'A','ි':'I','ී':'II','ු':'U','ූ':'UU','ෙ':'E','ො':'O', 
+       '්':'V'
+    };
 
+    const myanmar={
+        'က':'k','ခ':'K','ဂ':'g', 'ဃ':'G','င':'N', 'ဟ':'h',
+        'စ':'c','ဆ':'C','ဇ':'j','ဈ':'J','ဉ':'Y','ယ':'y','श':'Z',
+        'ဋ':'W','ဌ':'X','ဍ':'F','ဎ':'Q','ဏ':'N','ရ':'r','ष':'S',
+        'တ':'t','ထ':'T','ဒ':'d','ဓ':'D','န':'n','လ':'l','သ':'s',
+        'ပ':'p','ဖ':'P','ဗ':'b','ဘ':'B','မ':'m','ဝ':'v','ဠ':'L','ံ':'M',
+        'အ':'a','ဣ':'i','ဥ':'u','ဧ':'e','ဩ':'o','အာ':'aA','ဤ':'iI','ဦ':'uU',
+        'ာ':'A','ိ':'I','ီ':'II','ု':'U','ူ':'UU','ေ':'E','ော':'O',
+        '္':'V',
+        '၀':'0','၁':'1','၂':'2','၃':'3','၄':'4','၅':'5','၆':'6','၇':'7','၈':'8','၉':'9',
+    };
+    const thai={
+        'ก':'k','ข':'K','ค':'g', 'ฆ':'G','ง':'N', 'ห':'h', 
+        'จ':'c','ฉ':'C','ช':'j','ฌ':'J','ญ':'Y','ย':'y','श':'Z',
+        'ฏ':'W','ฐ':'X','ฑ':'F','ฒ':'Q','ณ':'N','ร':'r','ष':'S',
+        'ต':'t','ถ':'T','ท':'d','ธ':'D','น':'n','ล':'l','ส':'s',
+        'ป':'p','ผ':'P','พ':'b','ภ':'B','ม':'m','ว':'v','ฬ':'L','ํ':'M', 
+        'อ':'a','อิ':'i','อุ':'u','เอ':'e','โอ':'o','อา':'aA','อี':'iI','อู':'uU',
+        'า':'A','ิ':'I','ี':'II','ุ':'U','ู':'UU','เ':'E','โ':'O',
+        'ฺ':'V',
+        '๐':'0','๑':'1','๒':'2','๓':'3','๔':'4','๕':'5','๖':'6','๗':'7','๘':'8','๙':'9',
+    };
+    const khmer={
+
+        'ក':'k','ខ':'K','គ':'g', 'ឃ':'G','ង':'N', 'ហ':'h',
+       'ច':'c','ឆ':'C','ជ':'j','ឈ':'J','ញ':'Y','យ':'y','श':'Z',
+       'ដ':'W','ឋ':'X','ឌ':'F','ឍ':'Q','ណ':'N','រ':'r','ष':'S',
+       'ត':'t','ថ':'T','ទ':'d','ធ':'D','ន':'n','ល':'l','ស':'s',
+       'ប':'p','ផ':'P','ព':'b','ភ':'B','ម':'m','វ':'v','ឡ':'L','ំ':'M',
+       'អ':'a','ឥ':'i','ឧ':'u','ឯ':'e','ឱ':'o','អា':'aA','ឦ':'iI','ឩ':'uU',
+       'ា':'A','ិ':'I','ី':'II','ុ':'U','ូ':'UU','េ':'E','ោ':'O',
+          '្':'V',
+          '០':'0','១':'1','២':'2','៣':'3','៤':'4','៥':'5','៦':'6','៧':'7','៨':'8','៩':'9',
+    };
+    const lao={
+        'ກ':'k','ຂ':'K','ຄ':'g', 'ຆ':'G','ງ':'N', 'ຫ':'h',
+        'ຈ':'c','ຉ':'C','ຊ':'j','ຌ':'J','ຎ':'Y','ຍ':'y','श':'Z',
+        'ຏ':'W','ຐ':'X','ຑ':'F','ຒ':'Q','ຓ':'N','ຣ':'r','ष':'S',
+        'ຕ':'t','ຖ':'T','ທ':'d','ຘ':'D','ນ':'n','ລ':'l','ສ':'s',
+        'ປ':'p','ຜ':'P','ພ':'b','ຠ':'B','ມ':'m','ວ':'v','ຬ':'L','ໍ':'M',
+        'ອ':'a','ອິ':'i','ອຸ':'u','ເອ':'e','ໂອ':'o','ອາ':'aA','ອີ':'iI','ອູ':'uU',
+          'າ':'A','ິ':'I','ີ':'II','ຸ':'U','ູ':'UU','ເ':'E','ໂ':'O',
+       '຺':'V',
+         '໐':'0','໑':'1','໒':'2','໓':'3','໔':'4','໕':'5','໖':'6','໗':'7','໘':'8','໙':'9',
+    };
+    const tibetan={
+        'ཀ':'k','ཁ':'K','ག':'g', 'གྷ':'G','ང':'N', 'ཧ':'h',
+        'ཙ':'c','ཚ':'C','ཛ':'j','ཛྷ':'J','ཉ':'Y','ཡ':'y','श':'Z',
+        'ཊ':'W','ཋ':'X','ཌ':'F','ཌྷ':'Q','ཎ':'N','ར':'r','ष':'S',
+        'ཏ':'t','ཐ':'T','ད':'d','དྷ':'D','ན':'n','ལ':'l','ས':'s',
+        'པ':'p','ཕ':'P','བ':'b','བྷ':'B','མ':'m','ཝ':'v','ལ༹':'L','ཾ':'M',
+        'ཨ':'a','ཨི':'i','ཨུ':'u','ཨེ':'e','ཨོ':'o','ཨཱ':'aA','ཨཱི':'iI','ཨཱུ':'uU',
+        'ཱ':'A','ི':'I','ཱི':'II','ུ':'U','ཱུ':'UU','ེ':'E','ོ':'O',
+        '྄':'V', 
+        '༠':'0','༡':'1','༢':'2','༣':'3','༤':'4','༥':'5','༦':'6','༧':'7','༨':'8','༩':'9',
+    };
+    // export const cyrillic={
+    //     'к':'k','кх':'K','г':'g', 'гх':'G','н̇а':'N', 'х':'h', 
+    //   'ч':'c','чх':'C','дж':'j','джха':'J','н̃а':'Y','йа':'y','श':'Z',
+    //   'т̣а':'w','т̣ха':'x','д̣а':'f','д̣ха':'q','н̣а':'н','ра':'р','ष':'с',
+    // 'та':'т','тха':'т','да':'д','дха':'д','на':'н','ла':'л','са':'с',
+    //  'па':'п','пха':'п','ба':'б','бха':'б','ма':'м','ва':'в','л̣а':'л','м̣':'м',
+    //  'а':'а','и':'и','у':'у','е':'е','о':'о','а̄':'аа','ӣ':'ии','ӯ':'уу',
+    //  'а̄':'а','и':'и','ӣ':'ии','у':'у','ӯ':'уу','е':'е','о':'о', 
+    //   '':'в',  
+    // }
+
+    const inverseTable=tbl=>{
+        const out={};
+        for (let key in tbl) out[ tbl[key] ]=key;
+        return out;
+    };
+
+    const tables={
+        hi:inverseTable(devanagari), my:inverseTable(myanmar),
+        th:inverseTable(thai),       km:inverseTable(khmer),
+        lo:inverseTable(lao),       si:inverseTable(sinhala),
+        tb:inverseTable(tibetan) //,    cy:inverseTable(cyrillic),
+    };
+    const convertToIndic=(content,table)=>{ //pure text, no tag
+        let i=0,out=[];    while (i<content.length) {
+            let o= table[ (content[i]+content[i+1])];
+            if (o) {
+                i++;
+            } else o=table[content[i]];
+            if (o) out+=o; else out+=content[i];
+            i++;
+        }
+        return out;
+    };
+
+    const toIndic=(content,lang='hi')=>{
+        const table=tables[lang];
+        return table?convertToIndic(content,table):content;
+    };
+
+    //for importing CST
     const fromDevanagari=content=>{
         const tokens=content.split(/([ऀ-ॿ]+)/);
         let out='';
@@ -295,7 +345,7 @@ var providentpali = (function (exports) {
                 out+=tk;
             } else {
                 for (let i=0;i<tk.length;i++) {
-                    const ch=mapping[tk[i]];
+                    const ch=devanagari[tk[i]];
                     if (typeof ch=='undefined') {
                         console.log('wrong char',tk[i],tk);
                     } else {
@@ -304,7 +354,8 @@ var providentpali = (function (exports) {
                 }
             }
         });
-        out=out.replace(/‘‘/g,'“').replace(/’’/g,'”').replace(/\u200d/g,'');
+        // out=out.replace(/\u200d/g,'');
+        out=out.replace(/[ऀ-ॿ]/g,''); //drop all unknown
         return out;
     };
 
@@ -316,11 +367,12 @@ var providentpali = (function (exports) {
     exports.breakIASTSyllable = breakIASTSyllable;
     exports.breakSyllable = breakSyllable;
     exports.convertIASTSyllable = convertIASTSyllable;
+    exports.convertToIndic = convertToIndic;
     exports.fromDevanagari = fromDevanagari;
     exports.fromIAST = fromIAST;
     exports.isRomanized = isRomanized;
-    exports.toESpeak = toESpeak;
     exports.toIAST = toIAST;
+    exports.toIndic = toIndic;
 
     Object.defineProperty(exports, '__esModule', { value: true });
 
